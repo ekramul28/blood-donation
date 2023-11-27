54396b4c1773f561d919d242c1039c28af622b2a
Ekramul <mdekramulhassan168@gmail.com>
2023-11-27 19:10:29 +0600

    delete and update api and digine added

--- src/page/allDasbord/DashnoardHome/DashboardHome.jsx
+++ src/page/allDasbord/DashnoardHome/DashboardHome.jsx
@@ -3,7 +3,8 @@ import UseMyDonationRequests from "../../../hooks/UseMyDonationRequests";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import Welcome from "../../../shared/Welcome/Welcome";
 import { Link } from "react-router-dom";
-
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 const DashboardHome = () => {
     const [donner, refetch] = UseMyDonationRequests();
     const data = donner.slice(0, 3)
@@ -22,6 +23,31 @@ const DashboardHome = () => {
             });
             refetch()
         }
+
+    }
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/delete/${id}`)
+                if (res.data.deletedCount > 0) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                    refetch()
+                }
+
+            }
+        });
 
     }
     const handelCancel = async (id) => {
@@ -56,7 +82,9 @@ const DashboardHome = () => {
                                     <th>DonationDate </th>
                                     <th>donationTime </th>
                                     <th>Status</th>
+                                    <th>Edit</th>
                                     <th>Done</th>
+
 
                                 </tr>
                             </thead>
@@ -70,15 +98,25 @@ const DashboardHome = () => {
                                         <td>{pending?.donationDate}</td>
                                         <td>{pending?.donationTime}</td>
                                         <td>{pending?.status}</td>
-                                        {
-                                            (pending?.status === "inprogress") ? <>
-                                                <td className="">
-                                                    <button onClick={() => handelDone(pending._id)} className="btn text-white bg-green-500">Done</button>
-                                                    <button onClick={() => handelCancel(pending._id)} className="btn text-white bg-red-500">Cancel</button>
-                                                </td>
-                                            </> : ""
-                                        }
+                                        <td >
+                                            <div className="flex">
+                                                <Link to={`/dashboard/update/${pending._id}`}>
+                                                    <button className="btn bg-green-500 text-white "><FaEdit></FaEdit></button>
+                                                </Link>
+                                                <button onClick={() => handleDelete(pending._id)} className="btn bg-red-500 text-white "><MdDelete></MdDelete></button>
 
+                                            </div>
+                                        </td>
+                                        <td >
+                                            {
+                                                (pending?.status === "inprogress") ? <>
+
+                                                    <div className=" flex"><button onClick={() => handelDone(pending._id)} className="btn text-white bg-green-500">Done</button>
+                                                        <button onClick={() => handelCancel(pending._id)} className="btn text-white bg-red-500">Cancel</button></div>
+
+                                                </> : <div className="flex justify-center items-center top-4"> <h1>NotInprogress</h1></div>
+                                            }
+                                        </td>
                                     </tr>)
                                 }
 
--- src/page/allDasbord/my-donation-requests/donationRequests.jsx
+++ src/page/allDasbord/my-donation-requests/donationRequests.jsx
@@ -3,6 +3,7 @@ import UseMyDonationRequests from "../../../hooks/UseMyDonationRequests";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { FaEdit } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { MdDelete } from "react-icons/md";
 const MyDonationRequests = () => {
 
     const [donner, refetch] = UseMyDonationRequests()
@@ -24,6 +25,31 @@ const MyDonationRequests = () => {
         }
 
     }
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/delete/${id}`)
+                if (res.data.deletedCount > 0) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                    refetch()
+                }
+
+            }
+        });
+
+    }
     const handelCancel = async (id) => {
         const res = await axiosSecure.patch(`/request/cancel/${id}`);
         console.log(res.data)
@@ -40,10 +66,24 @@ const MyDonationRequests = () => {
         }
     }
     return (
-        <div className="overflow-x-auto">
+        <div className="overflow-x-auto mt-6">
+            <div><h1 className="text-3xl text-center mb-4 font-bold"> Filter Your  Data</h1></div>
+            <div className="flex">
+                <select className="select text-xl font-bold select-bordered  w-3/4  ">
+                    <option className="text-3xl p-4" disabled selected>Select Your Status</option>
+                    <option className="text-3xl" >pending</option>
+                    <option className="text-3xl" >inprogress</option>
+                    <option className="text-3xl" >done</option>
+                    <option className="text-3xl" >canceled</option>
+
+
+                </select>
+                <input className="w-1/4 text-white bg-green-500 btn" type="submit" value="Filter" />
+            </div>
             <table className="table">
                 {/* head */}
                 <thead>
+
                     <tr className="bg-red-500 text-white">
                         <th>#</th>
                         <th>Name</th>
@@ -67,19 +107,24 @@ const MyDonationRequests = () => {
                             <td>{pending?.donationDate}</td>
                             <td>{pending?.donationTime}</td>
                             <td>{pending?.status}</td>
-                            <td>
-                                <Link to={`/dashboard/update/${pending._id}`}>
-                                    <button className="btn bg-green-500 text-white"><FaEdit></FaEdit></button>
+                            <td className=" ">
+                                <div className="flex">
+                                    <Link to={`/dashboard/update/${pending._id}`}>
+                                        <button className="btn bg-green-500 text-white "><FaEdit></FaEdit></button>
+                                    </Link>
+                                    <button onClick={() => handleDelete(pending._id)} className="btn bg-red-500 text-white "><MdDelete></MdDelete></button>
+                                </div>
 
-                                </Link>
                             </td>
                             {
                                 (pending?.status === "inprogress") ? <>
-                                    <td className="flex gap-1">
-                                        <button onClick={() => handelDone(pending._id)} className="btn text-white bg-green-500">Done</button>
-                                        <button onClick={() => handelCancel(pending._id)} className="btn text-white bg-red-500">Cancel</button>
+                                    <td className="">
+                                        <div className="flex ">
+                                            <button onClick={() => handelDone(pending._id)} className="btn text-white bg-green-500">Done</button>
+                                            <button onClick={() => handelCancel(pending._id)} className="btn text-white bg-red-500">Cancel</button>
+                                        </div>
                                     </td>
-                                </> : ""
+                                </> : <div className="flex justify-center items-center top-4"> <h1>NotInprogress</h1></div>
                             }
                         </tr>)
                     }
